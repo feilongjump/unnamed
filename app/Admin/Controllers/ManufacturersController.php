8384dc0a30fcff83ab08616da1b85df75326a751
feilongjump <feilongjump@qq.com>
2021-08-06 18:04:21 +0800

    厂家管理创建数据时使用分步表单

--- app/Admin/Controllers/ManufacturersController.php
+++ app/Admin/Controllers/ManufacturersController.php
@@ -42,54 +42,103 @@ class ManufacturersController extends AdminController
     protected function form(): Form
     {
         return Form::make(Manufacturer::with(['contacts', 'banks']), function (Form $form) {
-            $form->tab('厂家信息', function (Form $form) {
-                $form->display('id');
-                $form->text('code')->required();
-                $form->text('name')->required();
-                $form->select('category')
-                    ->options([1 => '测试分类'])
-                    ->required();
-                $form->select('purchaser')
-                    ->options([1 => '小菜鸟'])
-                    ->required();
-                $form->text('address')->required();
-                $form->textarea('remarks')->rows(10);
+            if ($form->isEditing()) {
 
-                $form->display('created_at');
-                $form->display('updated_at');
-            })->tab('联系人信息', function (Form $form) {
+                $form->tab('厂家信息', function (Form $form) {
 
-                $form->hasMany('contacts', '', function (Form\NestedForm $form) {
+                    $form->display('id');
 
-                    // TODO: 等待此回复 https://github.com/jqhph/dcat-admin/issues/1417
-                    // 关联时需要自动翻译，使用以下模式
-                    // $form->text('contacts.name')->required();
-                    // 同时在主翻译文件中，增加翻译字段 https://github.com/jqhph/dcat-admin/issues/229
-                    $form->text('name')->required();
-                    $form->text('telephone')->required();
-                    $form->email('email')->required();
-                    $form->text('fax')->required();
-                    $form->switch('is_default')->default(false);
+                    $this->basic($form);
 
                     $form->display('created_at');
                     $form->display('updated_at');
-                })->useTable()->width(12);
+                })->tab('联系人信息', function (Form $form) {
 
-            })->tab('银行卡信息', function (Form $form) {
+                    $this->contacts($form);
 
-                $form->hasMany('banks', '', function (Form\NestedForm $form) {
+                })->tab('银行卡信息', function (Form $form) {
 
-                    $form->text('name')->required();
-                    $form->text('currency')->required();
-                    $form->text('account_name')->required();
-                    $form->text('account_number')->required();
-                    $form->text('account_bank')->required();
-                    $form->text('bank_address')->required();
-                    $form->text('company_address')->required();
+                    $this->banks($form);
+                });
 
-                })->useTable()->width(12);
+                return;
+            }
 
-            });
+            $form->multipleSteps()
+                ->add('厂家信息', function ($step) {
+                    $this->basic($step);
+                })
+                ->add('联系人信息', function ($step) {
+
+                    $this->contacts($step);
+
+                })
+                ->add('银行卡信息', function ($step) {
+
+                    $this->banks($step);
+
+                })
+                ->done();
         });
+    }
+
+    /**
+     * 基本信息
+     *
+     * @param $form
+     */
+    private function basic($form)
+    {
+        $form->text('code')->required();
+        $form->text('name')->required();
+        $form->select('category')
+            ->options([1 => '测试分类'])
+            ->required();
+        $form->select('purchaser')
+            ->options([1 => '小菜鸟'])
+            ->required();
+        $form->text('address')->required();
+        $form->textarea('remarks')->rows(10);
+    }
+
+    /**
+     * 联系人表单
+     *
+     * @param $form
+     */
+    private function contacts($form)
+    {
+        $form->hasMany('contacts', '', function (Form\NestedForm $form) {
+
+            // TODO: 等待此回复 https://github.com/jqhph/dcat-admin/issues/1417
+            // 关联时需要自动翻译，使用以下模式
+            // $form->text('contacts.name')->required();
+            // 同时在主翻译文件中，增加翻译字段 https://github.com/jqhph/dcat-admin/issues/229
+            $form->text('name')->required();
+            $form->text('telephone')->required();
+            $form->email('email')->required();
+            $form->text('fax')->required();
+            $form->switch('is_default')->default(false);
+        })->useTable()->width(12);
+    }
+
+    /**
+     * 银行信息表单
+     *
+     * @param $form
+     */
+    private function banks($form)
+    {
+        $form->hasMany('banks', '', function (Form\NestedForm $form) {
+
+            $form->text('name')->required();
+            $form->text('currency')->required();
+            $form->text('account_name')->required();
+            $form->text('account_number')->required();
+            $form->text('account_bank')->required();
+            $form->text('bank_address')->required();
+            $form->text('company_address')->required();
+
+        })->useTable()->width(12);
     }
 }
--- database/migrations/2021_08_05_202422_create_manufacturer_table.php
+++ database/migrations/2021_08_05_202422_create_manufacturer_table.php
@@ -20,7 +20,7 @@ class CreateManufacturerTable extends Migration
             $table->string('category')->default('')->comment('厂家分类');
             $table->bigInteger('purchaser')->comment('采购负责人');
             $table->string('address')->default('')->comment('厂家地址');
-            $table->string('remarks')->default('')->comment('备注');
+            $table->text('remarks')->nullable()->comment('备注');
             $table->timestamps();
             $table->softDeletes();
         });
@@ -33,6 +33,6 @@ class CreateManufacturerTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('manufacturer');
+        Schema::dropIfExists('manufacturers');
     }
 }
