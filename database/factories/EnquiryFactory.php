edd416ceb7d48fbbbec99baba41a6520881b0d49
feilongjump <feilongjump@qq.com>
2021-09-08 14:49:16 +0800

    :bug: 增加 en 语言生成器

--- database/factories/EnquiryFactory.php
+++ database/factories/EnquiryFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use App\Models\Enquiry;
+use Faker\Provider\Internet;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class EnquiryFactory extends Factory
@@ -21,6 +22,11 @@ class EnquiryFactory extends Factory
      */
     public function definition(): array
     {
+        // URL 在中文模式下无效
+        // 手动增加 en 语言生成器
+        $faker = \Faker\Factory::create('en');
+        $this->faker->addProvider(new Internet($faker));
+
         return [
             'manufacturer_id' => $this->faker->randomDigitNotNull,
             'manufacturer_contact_id' => $this->faker->randomDigitNotNull,
@@ -29,7 +35,7 @@ class EnquiryFactory extends Factory
             'pack' => $this->faker->word,
             'moq' => $this->faker->randomNumber(),
             'price' => $this->faker->randomFloat(2, 1, 99999),
-            'website' => $this->faker->url(),
+            'website' => $this->faker->url,
             'delivered_at' => $this->faker->dateTimeThisDecade('+3 months')
         ];
     }
