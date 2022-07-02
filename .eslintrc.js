990edee5ba00f60b48ecb5e48fe7a4b7ece526c4
feilongjump <feilongjump@qq.com>
2022-07-02 23:14:46 +0800

    init

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+    'vue/setup-compiler-macros': true
+  },
+  extends: ['plugin:vue/vue3-recommended', 'airbnb-base', 'plugin:prettier/recommended'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    parser: '@typescript-eslint/parser',
+    sourceType: 'module'
+  },
+  plugins: ['vue', '@typescript-eslint'],
+  rules: {
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        ts: 'never'
+      }
+    ],
+    'import/no-extraneous-dependencies': ['error', { devDependencies: ['vite.config.ts'] }],
+    'vue/multi-word-component-names': 'off',
+    'no-param-reassign': 'off',
+    'class-methods-use-this': 'off'
+  }
+}
