36138eff72378c00825039501b05ddb619d57dee
feilongjump <feilongjump@qq.com>
2022-07-04 07:54:26 +0800

    Navbar

--- src/layout/components/Navbar.vue
+++ src/layout/components/Navbar.vue
@@ -1,9 +1,27 @@
 <template>
-  <div class="navbar-container">Navbar</div>
+  <div class="navbar-container">
+    <div>
+      <ChevronDoubleLeftIcon class="w-5 h-5 cursor-pointer stroke-gray-500" />
+    </div>
+    <div class="nav-right">
+      <div class="keywords-input mr-6">
+        <el-input v-model="keywords" placeholder="Search..." :prefix-icon="SearchIcon" />
+      </div>
+      <BellIcon class="h-6 w-6 stroke-blue-400 mr-6 cursor-pointer" />
+      <ChatIcon class="h-6 w-6 stroke-indigo-400 mr-6 cursor-pointer" />
+      <img class="h-10 w-10 rounded-full" :src="avatar" />
+    </div>
+  </div>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { BellIcon, ChatIcon, ChevronDoubleLeftIcon, SearchIcon } from '@heroicons/vue/outline'
+import avatar from '@/assets/avatar.png'
+import { ref } from 'vue'
 
-<style scoped>
+const keywords = ref('')
+</script>
+
+<style>
 @import '@/styles/backstage/layout/navbar.css';
 </style>
